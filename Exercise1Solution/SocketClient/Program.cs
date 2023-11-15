7d95b76ef21a3330c4569fb92f86b9bb5f5e3413
Ștefan Cadar <65918883+StefanFaceCod2903@users.noreply.github.com>
2023-11-15 11:29:30 +0200

    initial commit

---
+++ Exercise1Solution/SocketClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Programm
+    {
+        static Socket socket;
+        static void Main(string[] args)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+            try
+            {
+                socket.Connect(localEndPoint);
+            }
+            catch
+            {
+                Console.WriteLine("Client is unable to connect");
+                socket.Close();
+                System.Environment.Exit(0);
+            }
+            Console.WriteLine("Enter text:");
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if (text.StartsWith("#"))
+                {
+                    if (text == "#quit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid command.");
+                        continue;
+                    }
+                }
+                byte[] data = Encoding.ASCII.GetBytes(text);
+                socket.Send(data);
+            }
+            socket.Close();
+        }
+    }
+}
---
+++ Exercise1Solution/SocketServer/Server.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SockerServer
+{
+    class Program
+    {
+        static Byte[] Buffer { get; set; }
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(IPAddress.Any, 1234));
+            sck.Listen(100);
+            bool open = true;
+            Socket accepted = sck.Accept();
+            Thread backgroundThread = new Thread(new ThreadStart(()=>
+            {
+                while (open) {
+                    string command = Console.ReadLine();
+                    if (command == "#quit") {
+                        open = false;
+                    } else
+                    {
+                        Console.WriteLine("Command not valid! Write '#quit to quit'");
+                    }
+                }
+            }));
+            backgroundThread.Start();
+            while (open)
+            {
+                Buffer = new Byte[accepted.SendBufferSize];
+                int bytesRead = accepted.Receive(Buffer);
+
+                byte[] formatted = new byte[bytesRead];
+                for (int i = 0; i < formatted.Length; i++)
+                {
+                    formatted[i] = Buffer[i];
+                }
+                string data = Encoding.ASCII.GetString(formatted);
+                Console.WriteLine(data);
+            }
+            accepted.Close();
+            sck.Close();
+        }
+    }
+}
